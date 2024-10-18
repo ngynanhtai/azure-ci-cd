6f67e64f30dff9c39963f9d980954dd984c502b7
ngynanhtai <ngynanhtai@gmail.com>
2024-10-19 02:34:21 +0700

    update app name

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-app-python.azurewebsites.net:$PORT/predict 
+     -X POST https://udacity-project-02.azurewebsites.net:$PORT/predict 
