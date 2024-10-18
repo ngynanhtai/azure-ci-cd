4f2afa6643804d9abbdbd96b4b869addc193c68c
ngynanhtai <82513252+ngynanhtai@users.noreply.github.com>
2024-10-19 00:20:23 +0700

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://udacity-project-2.azurewebsites.net:$PORT/predict
+     -X POST https://flash-python-app.azurewebsites.net:$PORT/predict
