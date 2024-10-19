5424bb73e7cf177ae8f51b9a4bf6f71571ba1fd5
ngynanhtai <ngynanhtai@gmail.com>
2024-10-19 09:21:41 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
+import os
 
 import pandas as pd
 # from sklearn.externals import joblib
@@ -30,10 +31,8 @@ def predict():
     # Performs an sklearn prediction
 
     try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Housing_price_model", "LinearRegression.joblib")
+        clf = joblib.load(model_path)
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
